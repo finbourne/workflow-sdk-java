5cd8defc1654be7f3e0945098942318e0eae24d5
Concourse <concourse@finbourne.com>
2025-07-28 12:08:28 +0000

    workflow java updates for v2.0.223
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/workflow/ApiClient.java
+++ sdk/src/main/java/com/finbourne/workflow/ApiClient.java
@@ -212,7 +212,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.222/java");
+        setUserAgent("OpenAPI-Generator/2.0.223/java");
 
         authentications = new HashMap<String, Authentication>();
     }
